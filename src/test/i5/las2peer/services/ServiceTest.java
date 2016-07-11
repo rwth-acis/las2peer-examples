91ad772f5984abeb04061ee9fff62fd1b2842a6c
Jasper Nalbach <nalbach@dbis.rwth-aachen.de>
2016-07-11 10:50:59 +0200

    port used for tests is blocked on our jenkins (perhaps a process crashed), now a free port is chosen automatically

--- src/test/i5/las2peer/services/ServiceTest.java
+++ src/test/i5/las2peer/services/ServiceTest.java
@@ -3,14 +3,6 @@ package i5.las2peer.services;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 import i5.las2peer.p2p.LocalNode;
 import i5.las2peer.p2p.ServiceNameVersion;
 import i5.las2peer.security.ServiceAgent;
@@ -21,6 +13,15 @@ import i5.las2peer.webConnector.WebConnector;
 import i5.las2peer.webConnector.client.ClientResponse;
 import i5.las2peer.webConnector.client.MiniClient;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 /**
  * Example Test Class demonstrating a basic JUnit test structure.
  *
@@ -28,7 +29,7 @@ import i5.las2peer.webConnector.client.MiniClient;
 public class ServiceTest {
 
 	private static final String HTTP_ADDRESS = "http://127.0.0.1";
-	private static final int HTTP_PORT = WebConnector.DEFAULT_HTTP_PORT;
+	private static int HTTP_PORT = WebConnector.DEFAULT_HTTP_PORT;
 
 	private static LocalNode node;
 	private static WebConnector connector;
@@ -38,9 +39,21 @@ public class ServiceTest {
 	private static final String testPass = "adamspass";
 
 	// during testing, the specified service version does not matter
-	private static final ServiceNameVersion testExampleService = new ServiceNameVersion(ExampleService.class.getCanonicalName(),"1.0");
+	private static final ServiceNameVersion testExampleService = new ServiceNameVersion(
+			ExampleService.class.getCanonicalName(), "1.0");
 
 	private static final String mainPath = "example/";
+
+	private static int getFreePort() {
+		int port = HTTP_PORT;
+		try {
+			ServerSocket socket = new ServerSocket(0);
+			port = socket.getLocalPort();
+			socket.close();
+		} catch (IOException e) {
+		}
+		return port;
+	}
 
 	/**
 	 * Called before the tests start.
@@ -51,11 +64,12 @@ public class ServiceTest {
 	 */
 	@BeforeClass
 	public static void startServer() throws Exception {
+		HTTP_PORT = getFreePort();
 
 		// start node
 		node = LocalNode.newNode();
 		testAgent = MockAgentFactory.getAdam();
-		testAgent.unlockPrivateKey(testPass); // agent must be unlocked in order to be stored 
+		testAgent.unlockPrivateKey(testPass); // agent must be unlocked in order to be stored
 		node.storeAgent(testAgent);
 		node.launch();
 
@@ -75,20 +89,17 @@ public class ServiceTest {
 
 		connector.updateServiceList();
 		// avoid timing errors: wait for the repository manager to get all services before continuing
-		try
-		{
+		try {
 			System.out.println("waiting..");
 			Thread.sleep(10000);
-		} catch (InterruptedException e)
-		{
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 
 	}
 
 	/**
-	 * Called after the tests have finished.
-	 * Shuts down the server and prints out the connector log file for reference.
+	 * Called after the tests have finished. Shuts down the server and prints out the connector log file for reference.
 	 * 
 	 * @throws Exception
 	 */
@@ -116,20 +127,17 @@ public class ServiceTest {
 	 * 
 	 */
 	@Test
-	public void testValidateLogin()
-	{
+	public void testValidateLogin() {
 		MiniClient c = new MiniClient();
 		c.setAddressPort(HTTP_ADDRESS, HTTP_PORT);
 
-		try
-		{
+		try {
 			c.setLogin(Long.toString(testAgent.getId()), testPass);
 			ClientResponse result = c.sendRequest("GET", mainPath + "validation", "");
-            assertEquals(200, result.getHttpCode());
+			assertEquals(200, result.getHttpCode());
 			assertTrue(result.getResponse().trim().contains("adam")); // login name is part of response
 			System.out.println("Result of 'testValidateLogin': " + result.getResponse().trim());
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 			fail("Exception: " + e);
 		}
@@ -138,18 +146,15 @@ public class ServiceTest {
 
 	/**
 	 * 
-	 * Test the example method that consumes one path parameter
-	 * which we give the value "testInput" in this test.
+	 * Test the example method that consumes one path parameter which we give the value "testInput" in this test.
 	 * 
 	 */
 	@Test
-	public void testExampleMethod()
-	{
+	public void testExampleMethod() {
 		MiniClient c = new MiniClient();
 		c.setAddressPort(HTTP_ADDRESS, HTTP_PORT);
 
-		try
-		{
+		try {
 			c.setLogin(Long.toString(testAgent.getId()), testPass);
 			ClientResponse result = c.sendRequest("POST", mainPath + "myResourcePath/testInput", ""); // testInput is
 																										// the pathParam
@@ -163,12 +168,10 @@ public class ServiceTest {
 	}
 
 	/**
-	 * Test the ExampleService for valid rest mapping.
-	 * Important for development.
+	 * Test the ExampleService for valid rest mapping. Important for development.
 	 */
 	@Test
-	public void testDebugMapping()
-	{
+	public void testDebugMapping() {
 		ExampleService cl = new ExampleService();
 		assertTrue(cl.debugMapping());
 	}
